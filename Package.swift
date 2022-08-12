d9d283752b45561aff79be9ead59bb61f48762b6
lovee <shonetesmith@gmail.com>
2022-08-13 03:07:38 +0900

    Fix package decreption

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftCloudHelloWorld",
+    platforms: [
+        .macOS(.v10_15),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
